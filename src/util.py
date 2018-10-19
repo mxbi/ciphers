d6cc1a576546c32a06d2e85914f26212b4eaab7a
Salvatore Elia <salvatore@totietje.net>
2018-10-19 16:04:00 +0100

    Added english frequncies

--- src/util.py
+++ src/util.py
@@ -1,8 +1,35 @@
 from functools import reduce
 
-alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-frequencies = {}
+frequencies = {
+	'a': 0.08167,
+	'b': 0.01492,
+	'c': 0.02782,
+	'd': 0.04253,
+	'e': 0.12702,
+	'f': 0.02228,
+	'g': 0.02015,
+	'h': 0.06094,
+	'i': 0.06966,
+	'j': 0.00153,
+	'k': 0.00772,
+	'l': 0.04025,
+	'm': 0.02406,
+	'n': 0.06749,
+	'o': 0.07507,
+	'p': 0.01929,
+	'q': 0.00095,
+	'r': 0.05987,
+	's': 0.06327,
+	't': 0.09056,
+	'u': 0.02758,
+	'v': 0.00978,
+	'w': 0.02360,
+	'x': 0.00150,
+	'y': 0.01974,
+	'z': 0.00074,
+}
 
 
 # Euclid's algorithm to find gcd of a list of numbers
@@ -17,6 +44,8 @@ def gcd(*args):
 	return reduce(gcd_two, args)
 
 
+# Given a string, creates a map of letters vs number of times the letter appears in the text
+# eg 'aabccc' returns {'a': 2, 'b': 1, 'c': 3} with every other letter mapping to 0.
 def letter_counts(text):
 	return dict((letter, text.count(letter)) for letter in alphabet)
 
